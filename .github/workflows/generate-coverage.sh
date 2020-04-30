2075ee1c1fb087992cfc674f4ff282b66f7e7f9b
Akshay Nair <phenax5@gmail.com>
2020-04-30 22:19:25 +0530

    Coverage shit refactor

--- .github/workflows/generate-coverage.sh
+++ .github/workflows/generate-coverage.sh
@@ -4,6 +4,20 @@ bin_dir=target/debug;
 cov_dir=target/cov;
 kcov=$(which kcov || echo "kcov-master/build/src/kcov");
 
+root=$(pwd);
+
+install_kcov() {
+  sudo apt-get install libcurl4-openssl-dev libelf-dev libdw-dev cmake gcc binutils-dev libiberty-dev zlib1g-dev;
+
+  wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz && \
+    tar xzf master.tar.gz && \
+    cd kcov-master && \
+    mkdir build && \
+    cd build && \
+    cmake .. && \
+    make;
+}
+
 report_cov() {
   ./$kcov --exclude-pattern=/.cargo,/usr/lib --verify $cov_dir "$1";
 }
@@ -12,5 +26,10 @@ get_binaries() {
   find $bin_dir -maxdepth 1 -perm -111 -type f;
 }
 
+# Install kcov if required
+which kcov || install_kcov;
+cd $root;
+
+# Build report
 get_binaries | while read bin; do report_cov $bin; done;
 
--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -7,14 +7,3 @@ rustup component add clippy --toolchain=nightly;
 
 cargo install cargo-cmd;
 
-
-sudo apt-get install libcurl4-openssl-dev libelf-dev libdw-dev cmake gcc binutils-dev libiberty-dev zlib1g-dev;
-
-wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz && \
-tar xzf master.tar.gz && \
-cd kcov-master && \
-mkdir build && \
-cd build && \
-cmake .. && \
-make && \
-sudo make install;
