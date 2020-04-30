bb116a3756cbeba769be82ae0884192bed45ed31
Akshay Nair <phenax5@gmail.com>
2020-04-30 15:01:15 +0530

    fucking work dude

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sh <(curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs) -y && \
+  rustup update stable && \
   rustup toolchain install nightly && \
   rustup component add clippy --toolchain=nightly;
