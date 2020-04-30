dcf041a511ff6fa49185bb245906a7d5035cf08d
Akshay Nair <phenax5@gmail.com>
2020-04-30 16:40:51 +0530

    It is getting really fucking annoying now

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -7,4 +7,14 @@ rustup component add clippy --toolchain=nightly;
 
 cargo install cargo-cmd;
 
-sudo apt-get install kcov;
+
+sudo apt-get install libcurl4-openssl-dev libelf-dev libdw-dev cmake gcc binutils-dev libiberty-dev zlib1g-dev;
+
+wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz && \
+tar xzf master.tar.gz && \
+cd kcov-master && \
+mkdir build && \
+cd build && \
+cmake .. && \
+make && \
+sudo make install;
