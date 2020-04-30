61351e6ffc888964d999c78eec3c162b9513247d
Akshay Nair <phenax5@gmail.com>
2020-04-30 14:46:33 +0530

    Removes dumb code

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 sh <(curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs) -y && \
-sh <(curl https://sh.rustup.rs -sSf) && \
 cargo install --git https://github.com/rust-lang/rust-clippy/ --force clippy;
