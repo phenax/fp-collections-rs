1c5eeab8737dcdd45ca0252123ebef4cced1ae88
Akshay Nair <phenax5@gmail.com>
2020-04-30 15:03:58 +0530

    Removing rustup reinstall

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sh <(curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs) -y && \
-  rustup update stable && \
-  rustup toolchain install nightly && \
-  rustup component add clippy --toolchain=nightly;
+# sh <(curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs) -y && \
+
+rustup update stable;
+
+rustup toolchain install nightly && \
+rustup component add clippy --toolchain=nightly;
