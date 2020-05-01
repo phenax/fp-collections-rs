a5906f7513e997582bd25e6bdad64634d0dcffb4
Akshay Nair <phenax5@gmail.com>
2020-05-01 17:48:02 +0530

    Adds fmt from rustup

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -4,6 +4,7 @@ rustup update stable;
 
 rustup toolchain install nightly && \
 rustup component add clippy --toolchain=nightly;
+rustup component add rustfmt --toolchain=nightly;
 
 cargo install cargo-cmd;
 
