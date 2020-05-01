04b2a704312eae9befe47a9145ab361cc628a514
Akshay Nair <phenax5@gmail.com>
2020-05-01 12:28:40 +0530

    Adds constant and compose2

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,20 +1,9 @@
 
 pub fn identity<T>(x: T) -> T { x }
 
-// pub fn constant<T>(x: T) -> impl Fn() -> T { || x }
+pub fn constant<T: Clone>(x: T) -> impl Fn() -> T { move || x.clone() }
 
-// macro_rules! compose {
-    // ( $last:expr ) => { $last };
-    // ( $head:expr, $($tail:expr), +) => {
-        // compose_two($head, compose!($($tail),+))
-    // };
-// }
-
-// fn compose_two<A, B, C, G, F>(f: F, g: G) -> impl Fn(A) -> C
-// where
-    // F: Fn(A) -> B,
-    // G: Fn(B) -> C,
-// {
-    // move |x| g(f(x))
-// }
+pub fn compose2<T, V, R>(fn1: impl Fn(V) -> R, fn2: impl Fn(T) -> V) -> impl Fn(T) -> R {
+    move |x| fn1(fn2(x))
+}
 
--- tests/helpers.rs
+++ tests/helpers.rs
@@ -25,3 +25,45 @@ pub mod identity {
         assert_eq!(list, result);
     }
 }
+
+pub mod constant {
+    use fp_collections::{helpers::*};
+    use fp_collections::{list::{List}, ls};
+
+    #[test]
+    fn it_returns_a_getter() {
+        let object = 23;
+        assert_eq!(constant(object)(), object);
+
+        let object = "hello";
+        assert_eq!(constant(object)(), object);
+
+        let object = &[1, 2, 3];
+        assert_eq!(constant(object)(), object);
+
+        let object = ls![1, 2, 3];
+        assert_eq!(constant(object.clone())(), object);
+    }
+
+    // TODO: Add ability to use in a pointfree way
+    // #[test]
+    // fn it_can_be_used_pointfree() {
+        // let list = ls![1, 2, 3];
+        // let result = list.clone().map(constant(5));
+        // assert_eq!(list, ls![5, 5, 5]);
+    // }
+}
+
+pub mod compose2 {
+    use fp_collections::{helpers::*};
+    
+    #[test]
+    fn it_composes_same_types() {
+        fn add5(x: i32) -> i32 { x + 5 }
+        fn mul2(x: i32) -> i32 { x * 2 }
+
+        let do_math = compose2(add5, mul2);
+
+        assert_eq!(do_math(5), 15);
+    }
+}
